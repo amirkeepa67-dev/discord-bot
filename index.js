4a63b466f2bf9501e98448faea95daff1aaea047
amirkeepa67-dev <amirkeepa67@gmail.com>
2026-06-02 21:38:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+const {
+Client,
+GatewayIntentBits,
+SlashCommandBuilder,
+Routes
+} = require('discord.js');
 
-
-const { Client, GatewayIntentBits } = require('discord.js');
+const { REST } = require('@discordjs/rest');
 const express = require('express');
 
 const app = express();
@@ -17,8 +22,40 @@ const client = new Client({
 intents: [GatewayIntentBits.Guilds]
 });
 
+const commands = [
+new SlashCommandBuilder()
+.setName('ping')
+.setDescription('Replies with Pong!')
+.toJSON()
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+try {
+console.log('Registering slash commands...');
+
+await rest.put(
+Routes.applicationCommands('1511366077077258351'),
+{ body: commands }
+);
+
+console.log('Slash commands registered');
+} catch (error) {
+console.error(error);
+}
+})();
+
 client.once('ready', () => {
 console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('interactionCreate', async interaction => {
+if (!interaction.isChatInputCommand()) return;
+
+if (interaction.commandName === 'ping') {
+await interaction.reply('Pong!');
+}
 });
 
 client.login(process.env.TOKEN);
