5ceb635e120657094ad846aded70887413bd9b06
amirkeepa67-dev <amirkeepa67@gmail.com>
2026-06-02 21:44:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,17 @@ new SlashCommandBuilder()
 .setName('ping')
 .setDescription('Replies with Pong!')
 .toJSON()
+  const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+intents: [GatewayIntentBits.Guilds]
+});
+
+client.once('ready', () => {
+console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
 ];
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
